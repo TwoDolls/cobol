ad5c61d4beaaf97e2d06d1c8d32e29483975c8dc
TwoDolls <sim.nussbaum@gmail.com>
2016-02-18 15:12:14 +0100

    1

--- App.cob
+++ App.cob
@@ -2,6 +2,5 @@ Vérifier bien que vous arrivez à upload depuis le client Github
 en vérifiant à l'adresse https://github.com/TwoDolls/cobol en ouvrant App.cob
 que votre 'X' à été ajouté :
 
-Simon    : X
 Théo     : 
 Laétitia : 
